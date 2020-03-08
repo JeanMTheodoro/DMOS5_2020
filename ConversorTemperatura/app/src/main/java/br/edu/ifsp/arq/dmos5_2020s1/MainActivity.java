019f6383c0459d3f10c3b4ff89de34563c44ac9c
JeanMTheodoro <jean14silva2009@gmail.com>
2020-03-08 14:22:18 -0300

    VERSÃO 1

---
+++ ConversorTemperatura/app/src/main/java/br/edu/ifsp/arq/dmos5_2020s1/MainActivity.java
@@ -0,0 +1,93 @@
+package br.edu.ifsp.arq.dmos5_2020s1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button bntCelsius;
+    private EditText editextTemperature;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bntCelsius = findViewById(R.id.button_celsius);
+        editextTemperature = findViewById(R.id.editText);
+        textView = findViewById(R.id.textview);
+
+        bntCelsius.setOnClickListener(this);
+        editextTemperature.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(bntCelsius == v){
+
+            conveterCelsius();
+
+        }else if(editextTemperature == v){
+
+            clearTextview();
+        }
+
+    }
+
+    private void conveterCelsius() {
+
+
+        double temperature;
+
+        try {
+
+            temperature = readInput();
+
+        }catch (NumberFormatException e){
+
+            temperature = 0;
+            showExeption();
+        }
+
+        textView.setText(String.format("%.2f °C",(temperature-32)/1.8));
+    }
+
+    private void clearTextview() {
+
+        textView.setText("");
+    }
+
+
+    private double readInput()  throws NumberFormatException {
+
+        double input;
+
+        try {
+
+            input = Double.valueOf(editextTemperature.getText().toString());
+
+        }catch (NumberFormatException e){
+            showExeption();
+            input = 0;
+            throw e;
+
+        }
+
+        return input;
+
+    }
+
+    private void showExeption() {
+
+        Toast.makeText(this, "Digito inválido", Toast.LENGTH_SHORT).show();
+
+    }
+}
